3b2802fd79c9e0c0a1a7ffe6cc7a9dadeb81b9de
Guenton Brooks <Guenton.Brooks@ibis-management.com>
2024-12-12 10:55:56 -0400

    added mobile moving background

--- src/lib/components/containers/HomeContainer.svelte
+++ src/lib/components/containers/HomeContainer.svelte
@@ -4,7 +4,18 @@
 <div class="h-screen">
 	<!-- Small Screen -->
 	<div class="block md:hidden">
-		<div class="h-screen flex bg-cover bg-center bg-no-repeat bg-fixed home-background">
+		<div class="h-screen flex bg-cover bg-center bg-no-repeat bg-fixed">
+			<video
+				autoplay
+				loop
+				muted
+				playsinline
+				disablepictureinpicture
+				class="absolute w-full h-full object-cover -z-10 pr-5 pb-6"
+			>
+				<source src="/vid/glowshowBackgroundMobile.mp4" type="video/mp4" />
+			</video>
+
 			<div
 				class="flex-1 h-full bg bg-gradient-to-b from-surface-900 via-surface-900/30 to-surface-900"
 			>
@@ -42,8 +53,8 @@
 	</div>
 </div>
 
-<style>
+<!-- <style>
 	.home-background {
 		background-image: url('/img/homeBackground.jpg');
 	}
-</style>
+</style> -->
