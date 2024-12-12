743581546ecbde5e9700c59d9b6ad1d9fc13403c
Guenton Brooks <Guenton.Brooks@ibis-management.com>
2024-12-12 10:15:19 -0400

    added full screen video hero

--- src/lib/components/containers/HomeContainer.svelte
+++ src/lib/components/containers/HomeContainer.svelte
@@ -15,7 +15,24 @@
 
 	<!-- Large Screen -->
 	<div class="hidden md:block">
-		<div class="h-screen flex bg-cover bg-center bg-no-repeat bg-fixed home-background">
+		<div class="h-screen flex bg-cover bg-center bg-no-repeat bg-fixed">
+			<!-- <div
+				class="flex-1 h-full bg bg-gradient-to-b from-surface-900 via-surface-900/30 to-surface-900"
+			>
+				<slot />
+			</div> -->
+
+			<video
+				autoplay
+				loop
+				muted
+				playsinline
+				disablepictureinpicture
+				class="absolute w-full h-full object-cover -z-10 pr-5 pb-6"
+			>
+				<source src="/vid/glowshowBackgroundClip.mp4" type="video/mp4" />
+			</video>
+
 			<div
 				class="flex-1 h-full bg bg-gradient-to-b from-surface-900 via-surface-900/30 to-surface-900"
 			>
